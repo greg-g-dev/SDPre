7f7a7bd989538d6e3799d8b6a870d2eb512eeb0d
greg-g-dev <geeper@protonmail.com>
2020-07-29 17:33:01 -0700

    completed Objective2 Lab1,Lab2,Lab3

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Greg Gauthier";
+    int age = 51;
+    String state = "Washington";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
