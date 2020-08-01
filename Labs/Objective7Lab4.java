99cc2c727440a773a373fa810d2ebff264185757
greg-g-dev <geeper@protonmail.com>
2020-08-01 11:51:51 -0700

    Completed Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int total = 0;
+
+    while (counter <= 20) {
+      total = total + counter;
+      counter = counter + 1;
+    }
+    System.out.println(total);
+  }
+}
