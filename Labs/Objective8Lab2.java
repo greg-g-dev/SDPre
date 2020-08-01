b1939f09cb4ab88ce6dc7565a97e72c0ce570e72
greg-g-dev <geeper@protonmail.com>
2020-08-01 14:06:22 -0700

    Completed Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int total = 0;
+    for (int i = 1; i <= 20; i++) {
+      total = total + i;
+    }
+    System.out.println(total);
+  }
+}
