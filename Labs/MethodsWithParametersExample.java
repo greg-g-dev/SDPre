0c0177bed64f2a110c2d289311d31c5675a76ae3
greg-g-dev <geeper@protonmail.com>
2020-08-02 11:18:10 -0700

    Updated MethodsWithParamtersExample with findSum

--- Labs/MethodsWithParametersExample.java
+++ Labs/MethodsWithParametersExample.java
@@ -5,6 +5,7 @@ public class MethodsWithParametersExample{
     Scanner kb = new Scanner(System.in);
     String item;
     int quantity;
+    double num1, num2;
     System.out.println("Please enter your inventory item: ");
     item = kb.nextLine();
     System.out.println("Please enter the quantity: ");
@@ -12,9 +13,23 @@ public class MethodsWithParametersExample{
     printInventory(item, quantity);
 
     printInventory("tomatoes", 12);
+
+    System.out.println("Please give me one decimal number: ");
+    num1 = kb.nextDouble();
+    System.out.println("Please give me another decimal number: ");
+    num2 = kb.nextDouble();
+
+    double answer = findSum(num1, num2);
+    System.out.println(num1 + " + " + num2 + " = " + answer);
+
   }
 
   public static void printInventory(String inventoryItem, int numberOfInventoryItems){
     System.out.println("There are currently " + numberOfInventoryItems + " " + inventoryItem + " left.");
+  }
+
+  public static double findSum(double x, double y) {
+    double sum = x + y;
+    return sum;
   }
 }
